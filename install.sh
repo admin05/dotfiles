72e5aad62630b8cc6810e8aee49be2ce1ed44362
rock <rock@rocks-Mac.local>
2015-10-16 14:54:04 +0800

    initial

---
+++ install.sh
@@ -0,0 +1,3 @@
+ln -s ~/Documents/dotfiles/zshrc ~/.zshrc
+ln -s ~/Documents/dotfiles/oh-my-zsh/frisk.zsh-theme ~/.oh-my-zsh/themes/frisk.zsh-theme
+ln -s ~/Documents/dotfiles/sublime/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
