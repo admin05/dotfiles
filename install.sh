02b64ab19e2517ecd8828ad2e140f1a20729f7ef
rock <rock@rocks-Mac.local>
2015-10-29 16:46:29 +0800

    安装脚本中增加重命名默认dotfiles的命令

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #关联dotfiles
+mv ~/.zshrc ~/.zshrc.bak
+mv ~/.oh-my-zsh/themes/frisk.zsh-theme ~/.oh-my-zsh/themes/frisk.zsh-theme.bak
+mv ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings.bak
 ln -s ~/Documents/dotfiles/oh-my-zsh/zshrc ~/.zshrc
 ln -s ~/Documents/dotfiles/oh-my-zsh/frisk.zsh-theme ~/.oh-my-zsh/themes/frisk.zsh-theme
 ln -s ~/Documents/dotfiles/sublime/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
